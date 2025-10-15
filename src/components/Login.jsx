b68f14d3bc9abd4bdff67f17d814dc167527cd4e
VUDUCQUY <quydky2k5@gmail.com>
2025-10-15 15:22:12 +0700

    merge remote main

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,36 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import "./css/dangnhap.css";
 
 export default function Login() {
+    const [showPassword, setShowPassword] = useState(false);
+
     return (
         <div className="login-container">
-            <h1 className="company-title">
-                CÔNG TY CỔ PHẦN NĂM THÀNH VIÊN A+88
-            </h1>
             <div className="login-box">
-                <div className="login-left">
-                    <img
-                        src={require("./css/anhnhom.png")}
-                        alt="illustration"
-                        className="login-img"
-                    />
-                </div>
+                <h1 className="company-title">
+                    CÔNG TY CỔ PHẦN NĂM THÀNH VIÊN A+88
+                </h1>
 
-                <div className="login-right">
-                    <img
-                        src={require("./css/ava2.png")}
-                        alt="avatar"
-                        className="login-avatar"
-                    />
-                    <h2 className="login-title">ĐĂNG NHẬP</h2>
-                    <form className="login-form">
-                        <input type="text" placeholder="Username" required />
-                        <input type="password" placeholder="Password" required />
-                        <button type="submit">Log in</button>
-                    </form>
-                    <a href="/forgot" className="forgot-link">
-                        Forgot password?
-                    </a>
+                <div className="login-body">
+                    <div className="login-left">
+                        <img
+                            src={require("./css/anhnhom.png")}
+                            alt="illustration"
+                            className="login-img"
+                        />
+                    </div>
+
+                    <div className="login-right">
+                        <img
+                            src={require("./css/ava2.png")}
+                            alt="avatar"
+                            className="login-avatar"
+                        />
+                        <h2 className="login-title">ĐĂNG NHẬP</h2>
+
+                        <form className="login-form">
+                            <div className="input-field">
+                                <img
+                                    src={require("./css/usericon.png")}
+                                    alt="user"
+                                    className="input-icon"
+                                />
+                                <input type="text" placeholder="Username" required />
+                            </div>
+
+                            <div className="input-field">
+                                <img
+                                    src={require("./css/keyicon.png")}
+                                    alt="key"
+                                    className="input-icon"
+                                />
+                                <input
+                                    type={showPassword ? "text" : "password"}
+                                    placeholder="Password"
+                                    required
+                                />
+                                <span
+                                    className="toggle-password"
+                                    onClick={() => setShowPassword(!showPassword)}
+                                >
+                  {showPassword ? "🙈" : "👁️"}
+                </span>
+                            </div>
+
+                            <button type="submit">Log in</button>
+                        </form>
+
+                        <a href="/forgot" className="forgot-link">
+                            Forgot password?
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/QuenPass.jsx
+++ src/components/QuenPass.jsx
@@ -3,34 +3,58 @@ import "./css/quenpass.css";
 
 export default function QuenPass() {
     return (
-        <div className="forgot-container">
-            <h1 className="company-title">CÔNG TY CỔ PHẦN NĂM THÀNH VIÊN A+88</h1>
-            <div className="forgot-box">
-                <div className="forgot-left">
-                    <img
-                        src={require("./css/anhnhom.png")}
-                        alt="illustration"
-                        className="forgot-img"
-                    />
-                </div>
-                <div className="forgot-right">
-                    <a href="/" className="back-link">
-                        Quay lại
-                    </a>
-                    <img
-                        src={require("./css/ava1.png")}
-                        alt="avatar"
-                        className="forgot-avatar"
-                    />
-                    <h2>QUÊN MẬT KHẨU</h2>
-                    <form className="forgot-form">
-                        <input type="text" placeholder="Username" required />
-                        <input type="email" placeholder="Email" required />
-                        <button type="submit">XÁC NHẬN</button>
-                    </form>
-                    <p className="note">
-                        Chú ý: Hệ thống sẽ gửi mật khẩu mới qua email
-                    </p>
+        <div className="login-container">
+            <div className="login-box">
+                <h1 className="company-title">
+                    CÔNG TY CỔ PHẦN NĂM THÀNH VIÊN A+88
+                </h1>
+
+                <div className="login-body">
+                    <div className="login-left">
+                        <img
+                            src={require("./css/anhnhom.png")}
+                            alt="illustration"
+                            className="login-img"
+                        />
+                    </div>
+
+                    <div className="login-right">
+                        <a href="/" className="back-link">
+                            Quay lại
+                        </a>
+                        <img
+                            src={require("./css/ava1.png")}
+                            alt="avatar"
+                            className="login-avatar"
+                        />
+                        <h2 className="login-title">QUÊN MẬT KHẨU</h2>
+
+                        <form className="login-form">
+                            <div className="input-field">
+                                <img
+                                    src={require("./css/usericon.png")}
+                                    alt="user"
+                                    className="input-icon"
+                                />
+                                <input type="text" placeholder="Username" required />
+                            </div>
+
+                            <div className="input-field">
+                                <img
+                                    src={require("./css/mailicon.png")}
+                                    alt="email"
+                                    className="input-icon"
+                                />
+                                <input type="email" placeholder="Email" required />
+                            </div>
+
+                            <button type="submit">XÁC NHẬN</button>
+                        </form>
+
+                        <p className="note">
+                            Chú ý: Hệ thống sẽ gửi mật khẩu mới qua email
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
