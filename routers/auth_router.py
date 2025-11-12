2a43f842266630016a84d00a657ad3c0e2988526
Đỗ Đình Tuyến <tuyendodinh14@gmail.com>
2025-11-13 00:02:13 +0700

    Update auth_router.py

--- routers/auth_router.py
+++ routers/auth_router.py
@@ -1,23 +1,5 @@
-from fastapi import APIRouter, Depends
-from pydantic import BaseModel
-from sqlalchemy.orm import Session
-from ..controllers import auth_controller
-from ..configs.database import get_db
+from fastapi import APIRouter
+from app.controllers import auth_controller
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
-
-class RegisterRequest(BaseModel):
-    username: str
-    password: str
-
-@router.post("/register")
-def register_user(data: RegisterRequest, db: Session = Depends(get_db)):
-    return auth_controller.register(db=db, username=data.username, password=data.password)
-
-class LoginRequest(BaseModel):
-    username: str
-    password: str
-
-@router.post("/login")
-def login_user(data: LoginRequest, db: Session = Depends(get_db)):
-    return auth_controller.login(db=db, username=data.username, password=data.password)
+router.include_router(auth_controller.router)
