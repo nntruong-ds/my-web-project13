c43802f6730fc62372fc8e0591b3399d5ac04124
Đỗ Đình Tuyến <tuyendodinh14@gmail.com>
2025-11-13 00:01:22 +0700

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,9 +1,20 @@
-from sqlalchemy import Column, Integer, String
-from ..configs.database import Base
+from sqlalchemy import Column, String, Enum
+from app.configs.database import Base
+import enum
+
+class RoleEnum(str, enum.Enum):
+    tonggiamdoc = "tonggiamdoc"
+    giamdoc_cn = "giamdoc_cn"
+    truongphong = "truongphong"
+    nhanvien = "nhanvien"
 
 class User(Base):
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, nullable=False)
-    password = Column(String, nullable=False)
-    role = Column(String, default="user")
+
+    TenDangNhap = Column(String(20), primary_key=True)
+    MatKhau = Column(String(255), nullable=False)
+    VaiTro = Column(Enum(RoleEnum), default="nhanvien")
+    HoTen = Column(String(100))
+    Email = Column(String(100))
+    SoDienThoai = Column(String(20))
+    DiaChi = Column(String(255))
