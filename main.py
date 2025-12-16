527d2b43fb38dd9f8038526d2cd2652e6c7549c1
Đỗ Đình Tuyến <tuyendodinh14@gmail.com>
2025-12-16 12:07:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from fastapi import FastAPI
-from app.routers import auth_router, employee_router, email_router
+from app.routers import auth_router, employee_router, email_router, cham_cong_router, salary_router, birthday_router
 from app.configs.database import init_db
 
-# Khởi tạo app FastAPI
 app = FastAPI(title="Employee Management API")
 
-# Khởi tạo database
 init_db()
 
-# Include router
+# Auth
 app.include_router(auth_router.router, prefix="/auth")
+
+# Employees
 app.include_router(employee_router.router)
+
+# Sinh nhật
+app.include_router(birthday_router.router)
+
+# Email
 app.include_router(email_router.router)
+
+# Attendance (Chấm công)
+app.include_router(cham_cong_router.router)
+
+# Salary
+app.include_router(salary_router.router)
