8c115c78c6f413bc938a1c60b9b5fc596ed8c8e1
Đỗ Đình Tuyến <tuyendodinh14@gmail.com>
2025-12-24 17:48:10 +0700

    Update salary_router.py

--- routers/salary_router.py
+++ routers/salary_router.py
@@ -1,10 +1,34 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+
 from app.configs.database import get_db
-from app.services.salary_service import tinh_luong
+from app.utils.deps import get_current_user
+from app.services.salary_service import get_salary
 
 router = APIRouter(prefix="/salary", tags=["Salary"])
 
-@router.get("/calculate")
-def calculate_salary(ma_nhan_vien: str, month: int, year: int, db: Session = Depends(get_db)):
-    return tinh_luong(db, ma_nhan_vien, month, year)
+
+@router.get("")
+def get_salary_api(
+    ma_nhan_vien: str,
+    thang: int,
+    nam: int,
+    db: Session = Depends(get_db)
+):
+    try:
+        return get_salary(db, ma_nhan_vien, thang, nam)
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
+
+
+@router.get("/me")
+def get_salary_me(
+    thang: int,
+    nam: int,
+    db: Session = Depends(get_db),
+    current_user = Depends(get_current_user)
+):
+    try:
+        return get_salary(db, current_user.TenDangNhap, thang, nam)
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
