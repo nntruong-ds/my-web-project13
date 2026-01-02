95215fd3e16e9719c16734d907963ac1f67a67d5
Đỗ Đình Tuyến <tuyendodinh14@gmail.com>
2026-01-03 02:15:26 +0700

    Delete app directory

--- app/controllers/employee_controller.py
+++
@@ -1,4 +0,0 @@
-from fastapi import APIRouter
-from app.routers import employee_router
-router = APIRouter()
-router.include_router(employee_router.router)
--- app/schemas/email_schema.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class InternalEmailRequest(BaseModel):
-    receiver_ma_nv: str
-    subject: str
-    content: str
